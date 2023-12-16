a3fbc48155e9d48a7beee757119219d353fcfa90
iolowookere217 <olowookere.isaaco@gmail.com>
2023-12-16 17:43:19 +0100

    completed signin and signup and routed all pages

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -4,20 +4,29 @@ import useSubmit from "../hooks/useSubmit";
 import Button from "../components/button";
 import Input from "../components/input";
 
-const Login = () => {
+const SignUp = ({ visible, onClose, switchToSignIn }) => {
   const { errors, register, handleSubmit } = useSubmit(LoginSchema);
-
   const onLogin = (data) => {
     console.log(data);
   };
+  const handleOnClose = (e) => {
+    if (e.target.id === "close-modal") onClose();
+  };
+
+  if (!visible) return null;
 
   return (
     <React.Fragment>
-      <section className="flex items-center justify-center mt-8 ">
-        <div className="flex flex-col gap-8 bg-[#fff] mx-auto md:mx-24 p-10 rounded-[12px] h-auto w-[28rem] md:max-w-[40%] ">
+      <section
+        id="close-modal"
+        onClick={handleOnClose}
+        className="flex items-center justify-center fixed inset-0 bg-black bg-opacity-80 backdrop-blur-0 z-40"
+      >
+        <div className="flex flex-col gap-8 bg-[#fff] mx-auto md:mx-24 p-10 rounded-[12px] h-auto w-[28rem] md:max-w-[40%]">
           <h1 className="text-primary md:text-4xl text-2xl font-bold ">
-            Sign In
+            Sign Up
           </h1>
+
           <p className="text-secondary text-sm md:text-base font-normal ">
             Enter your Account Details Below
           </p>
@@ -25,6 +34,16 @@ const Login = () => {
             className="flex flex-col gap-2 "
             onSubmit={handleSubmit(onLogin)}
           >
+            <Input
+              name="name"
+              type="text"
+              register={register}
+              errors={errors}
+              autoComplete="name"
+              placeholder="What should we call you?"
+              label="Full Name"
+              variant="primary"
+            />
             <Input
               name="email"
               type="email"
@@ -47,19 +66,23 @@ const Login = () => {
             />
             <div className="flex flex-col gap-2">
               <Button variant="primary" type="submit">
-                Sign In
+                Sign Up
               </Button>
+              <p className="text-lg text-center text-secondary">
+                Already have an account?{" "}
+                <span
+                  className="text-tetiary font-bold cursor-pointer"
+                  onClick={switchToSignIn}
+                >
+                  Sign In
+                </span>
+              </p>
             </div>
           </form>
-          <h5 className="text-lg text-center text-secondary ">
-            don't have an account?{" "}
-            <a className="text-tetiary  font-bold" href="/register">
-              Sign Up
-            </a>
-          </h5>
         </div>
       </section>
     </React.Fragment>
   );
 };
-export default Login;
+
+export default SignUp;
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,10 +1,18 @@
 import logo from "../../public/fixeet-logo.png";
 import Button from "./button";
 import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import Modal from "./modal"; // Import the Modal component
 
 const NavBar = () => {
+  const [showModal, setShowModal] = useState(false);
+  const handleModalClose = () => {
+    setShowModal(false);
+  };
+
   return (
     <div className="bg-light flex justify-between items-center px-8 py-4">
+      {showModal && <Modal showSignIn={true} onClose={handleModalClose} />}
       <div className="w-2/5 flex  justify-between items-center relative">
         <div>
           <img src={logo} alt="logo" className="" />
@@ -13,17 +21,31 @@ const NavBar = () => {
 
       <div className="flex gap-8 justify-end  ">
         <div className="hidden md:flex justify-between  items-center gap-8 font-bold ">
-          <Link className="flex cursor-pointer">Home</Link>
-          <Link className="flex cursor-pointer">About</Link>
-          <Link className=" flex w-[4rem] cursor-pointer ">Sign In</Link>
-          <Link className="flex w-[6rem] cursor-pointer  text-primary">
+          <Link to="/home" className="flex cursor-pointer">
+            Home
+          </Link>
+          <Link to="/about" className="flex cursor-pointer">
+            About
+          </Link>
+          <button
+            onClick={() => setShowModal(true)}
+            className=" flex w-[4rem] cursor-pointer text-tetiary ml-4"
+          >
+            Sign In
+          </button>
+          <Link
+            to="/all-reports"
+            className="flex w-[6rem] cursor-pointer  text-tetiary"
+          >
             All Reports
           </Link>
         </div>
 
-        <Button variant="primary" type="submit">
-          Report an Issue
-        </Button>
+        <Link to="/new-report">
+          <Button variant="primary" type="submit">
+            Report an Issue
+          </Button>
+        </Link>
       </div>
     </div>
   );
